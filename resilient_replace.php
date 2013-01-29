ebe172d7631efaa5a232f285b159925286e59de8
Pierre-Alexis de Solminihac <pa@solminihac.fr>
2013-01-29 01:11:08 +0100

    Ajout des options -i et --only-into-serialized

--- resilient_replace.php
+++ resilient_replace.php
@@ -3,19 +3,48 @@
 ini_set('memory_limit', -1);
 ini_set('max_execution_time', 0);
 if (empty($argv[1]) || empty($argv[2])) {
-    file_put_contents('php://stderr', 'Usage : resilient_replace.php <search> <replace> [<file>]');
+    file_put_contents('php://stderr', '
+Usage: resilient_replace.php <search> <replace> [<file>]
+
+Options:
+    -i
+        edit in place
+
+    --only-into-serialized
+        replace only into serialized data (do not replace into raw data)
+
+        ');
     die();
 }
 
+// extrait les options
+$options = array();
+foreach ($argv as $key => $arg) {
+    if (strpos($arg, '-') === 0) {
+        $options[$arg] = 1;
+        unset($argv[$key]);
+    }
+}
+$argv = array_values($argv);
+
+// recupere les parametres
 $search  = $argv[1];
 $replace = $argv[2];
-
 $content_from_stdin = 0;
+$filename = '';
 if (empty($argv[3])) {
     $content_from_stdin = 1;
+} else {
+    $filename = $argv[3];
 }
 
-function resilient_replace ($search, $replace, $subject, $only_into_serialized = true) {
+/*print_r($options);*/
+/*echo 'search: ' . $search . PHP_EOL;*/
+/*echo 'replace: ' . $replace . PHP_EOL;*/
+
+
+function resilient_replace ($search, $replace, $subject, $only_into_serialized = false) {
+    $str = $subject;
     $search_escaped = str_replace("\\", "\\\\\\\\\\\\\\\\\\\\", $search);
     $replace_escaped = str_replace("'", "\\'", $replace);
 
@@ -26,14 +55,16 @@ function resilient_replace ($search, $replace, $subject, $only_into_serialized =
 
     $delta = strlen($replace) - strlen($search);
     // $nb_matches = preg_match
+    file_put_contents('php://stderr', 'replace into serialized' . PHP_EOL);
     $str = preg_replace(
         '/s:(\d+):(\\\?)"(.*?)\\2";/e',
         "'s:' 
         . (intval('\\1', 10) + preg_match_all('/" . $search_escaped . "/', '\\3', \$dummy) * (" . $delta . ")) 
         . ':\\2\"' 
         . preg_replace('/" . $search_escaped . "/', '" . $replace_escaped . "', '\\3') . '\\2\";'",
-        $subject);
+        $str);
     if (!$only_into_serialized) {
+        file_put_contents('php://stderr', 'replace into raw' . PHP_EOL);
         $str = preg_replace('/' . $search . '/', $replace, $str);
     }
     return $str;
@@ -41,14 +72,22 @@ function resilient_replace ($search, $replace, $subject, $only_into_serialized =
 if ($content_from_stdin) {
     $handle = fopen('php://stdin', 'r');
 } else {
-    $handle = fopen($argv[3], 'r');
+    $handle = fopen($filename, 'r');
 }
 $buffer = '';
 while(!feof($handle)) {
     $buffer .= fgets($handle);
 }
 fclose($handle);
-$buffer = resilient_replace($search, $replace, $buffer);
-echo $buffer;
+$only_into_serialized = 0;
+if (!empty($options['--only-into-serialized'])) {
+    $only_into_serialized = 1;
+}
+$buffer = resilient_replace($search, $replace, $buffer, $only_into_serialized);
+if (!empty($options['-i'])) {
+    file_put_contents($filename, $buffer);
+} else {
+    echo $buffer;
+}
 
 ?>
